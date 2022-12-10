4c60b34d592ee2d653647bca83587a799006c25f
Uday Sharma <itsudayy@gmail.com>
2022-12-10 13:40:15 +0530

    basic routing setup and basic UI

--- client/urls.py
+++ client/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='client-home'),
     path('login/', views.login, name='client-login'),
-    path('register/', views.register, name='client-register')
+    path('register/', views.register, name='client-register'),
+    path('confessions/', views.confessions, name='client-confessions'),
+    path('chat/', views.chat, name='client-chat'),
+    path('profile/<int:profile_id>/', views.profile, name='client-profile')
 ]
--- client/views.py
+++ client/views.py
@@ -3,9 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {
-        "title": 'Home'
+        "title": 'Town Hall'
     }
     return render(request, 'client/home.html', context)
+
+
+def confessions(request):
+    context = {
+        "title": "Gossip Girl"
+    }
+    return render(request, 'client/confessions.html', context)
+
+
+def chat(request):
+    context = {
+        "title": "Chats"
+    }
+    return render(request, 'client/chat.html', context)
+
+
+def profile(request, profile_id):
+    name = "Soul King"
+    context = {
+        "title": "Profile",
+        "name": name,
+        "profile_id": profile_id
+    }
+    return render(request, 'client/profile.html', context)
 
 
 def login(request):
