e0f1c8f1fb1ec153be7b3c4e283551717f57b704
Uday Sharma <itsudayy@gmail.com>
2023-01-04 21:20:18 +0530

    only users with college email can register

--- users/views.py
+++ users/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 from .models import Student
+import re
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            new_user = form.save()
-            new_student = Student(user=new_user)
-            new_student.save()
-            first_name = form.cleaned_data.get('first_name')
-            messages.success(request, f"Account Created for {first_name}!\nTry Logging In")
-            return redirect('login')
+            if re.findall("@sggscc.ac.in", form.cleaned_data.get('email')):
+                new_user = form.save()
+                new_student = Student(user=new_user)
+                new_student.save()
+                first_name = form.cleaned_data.get('first_name')
+                messages.success(request, f"Account Created for {first_name}!\nTry Logging In")
+                return redirect('login')
+            else:
+                messages.error(request, f"Please Create Your Account With College Email")
+                return redirect('register')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form, "title": "Register"})
